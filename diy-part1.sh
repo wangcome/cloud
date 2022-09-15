7864bcbb9fd905aa776cbb7cc4b395f3d3f4c4e9
wangcome <58815948+wangcome@users.noreply.github.com>
2022-09-16 00:33:16 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,5 +19,5 @@
 #echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 #echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;packages' >>feeds.conf.default
-#echo 'src-git NueXini_Packages https://github.com/NueXini/NueXini_Packages.git' >>feeds.conf.default
+echo 'src-git NueXini_Packages https://github.com/NueXini/NueXini_Packages.git' >>feeds.conf.default
 echo 'src-git pl https://github.com/xiaorouji/openwrt-passwall.git;luci' >>feeds.conf.default
