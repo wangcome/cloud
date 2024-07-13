619430eafd30904aa8159ff61ce64dc8dd298856
wangcome <58815948+wangcome@users.noreply.github.com>
2024-07-14 00:24:25 +0800

    Update 456.cs

--- config/456.cs
+++ config/456.cs
@@ -3,7 +3,7 @@ IP地址	端口	TLS	数据中心	地区	城市	网络延迟	下载速度(M/s)
 15.165.49.161	8080	TRUE	ICN	Asia Pacific	Seoul	96 ms	12.09
 3.120.144.20	8080	TRUE	FRA	Europe	Frankfurt	179 ms	8.29
 13.250.131.37	443	TRUE	SIN	Asia Pacific	Singapore	103 ms	8.19
-18.167.194.154	443	TRUE	HKG	Asia Pacific	Hong Kong	137 ms	8.09
+18.167.194.154	443	TRUE	HKG	Asia Pacific	Hong Kong	137 ms	8.0
 
 
 
